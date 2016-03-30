064979bee28bfd56bed7f861174e4b8a37e73aaa
ZDDDD <362503813@qq.com>
2016-03-30 15:45:33 +0800

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,14 @@
+interface Animal extends LivingThing{
+    double weight = 4.5;
+    int lgs = 4;
+    void eat();
+    void travel();  
+}
+interface LivingThing {
+
+    void live();
+}
+interface Bird {
+    int swing = 2;
+    void fly();
+}
